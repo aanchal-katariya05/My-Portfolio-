40766d7b4ffab6460ca823d4978c9d0b25d55d45
Aanchal Katariya <2005katariyaaanchal@gmail.com>
2026-03-31 17:19:36 +0530

    v1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed = new typed (".text" , {
-    String : ["Fronted Developer" , "Youtube" , "Web Developer" ],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+ var typed = new Typed('#element', {
+      strings: ['Fronted Developer', ' Web Developer'],
+      typeSpeed: 100,
+      backSpeed:100,
+      backDelay:1000,
+      loop:true
+    });
